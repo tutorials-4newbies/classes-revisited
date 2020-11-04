8db0d1ea64a2b02fe39ff18ecbfb26d60aa0fdb1
alonisser <alonisser@gmail.com>
2020-11-05 00:28:47 +0200

    working with classes

--- game/game_intro.py
+++ game/game_intro.py
@@ -6,12 +6,31 @@ speed = [2, 2]  # this is a vector [left-right speed, top-down speed]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
-ball = pygame.image.load("./images/intro_ball.gif")
-ballrect = ball.get_rect()
 
-another_ball = False
-soccer_ball = False
-soccer_ball_rect = False
+
+class Ball:
+    def __init__(self, image_path, speed: list = None):
+        self.speed = speed or [2, 2]
+        self.ball = pygame.image.load(image_path)
+        self.rect = self.ball.get_rect()
+
+    def move(self):
+        self.rect = self.rect.move(self.speed)
+        if self.rect.left < 0 or self.rect.right > width:
+            self.speed[0] = -self.speed[0]
+        if self.rect.top < 0 or self.rect.bottom > height:
+            self.speed[1] = -self.speed[1]
+
+
+first_ball = Ball(image_path="./images/intro_ball.gif")
+
+balls = [first_ball]
+
+
+def add_ball(image_path: str = None):
+    image_path = image_path or "./images/soccer_ball.gif"
+    another_ball = Ball(image_path=image_path, speed=[1, 1])
+    balls.append(another_ball)
 
 
 while True:
@@ -19,27 +38,12 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                if not another_ball:
-                    another_ball = True
-                    soccer_ball = pygame.image.load("./images/soccer_ball.gif")
-                    soccer_ball_rect = soccer_ball.get_rect()
+            add_ball()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
-
-    if another_ball:
-        soccer_ball_rect = soccer_ball_rect.move(speed)
-        if soccer_ball_rect.left < 0 or soccer_ball_rect.right > width:
-            speed[0] = -speed[0]
-        if soccer_ball_rect.top < 0 or soccer_ball_rect.bottom > height:
-            speed[1] = -speed[1]
-
+    for ball in balls:
+        ball.move()
     screen.fill(black)
-    screen.blit(ball, ballrect)
-    if another_ball:
-        screen.blit(soccer_ball, soccer_ball_rect)
+    for ball in balls:
+        screen.blit(ball.ball, ball.rect)
+
     pygame.display.flip()
